b5199cfa45cb80f8ee0b5838059de910b13cdc4a
Rafa <rafazira83@gmail.com>
2018-11-17 17:03:08 -0300

    carga datos

--- BaseDato.js
+++ BaseDato.js
@@ -2,12 +2,14 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 app.get('/llenarBaseDatosPersonaje',function(){
-var personaje=[];
+var personaje=[{nombre:feredico,ruta:federico.jpg},{nombre:rafael,ruta:rafael.jpg},{nombre:diego,ruta:diego.jpg},
+    {nombre:maria,ruta:maria.jpg},{nombre:juan,ruta:juan.jpg},{nombre:sergio,ruta:sergio.jpg},{nombre:santino,ruta:santino.jpg},
+    {nombre:sergio,ruta:sergio.jpg}];
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
   for(var i=0;i<personajes.length;i++){
-  var myobj = { personaje: "Company Inc" };
+  var myobj = { personaje:  };
   dbo.collection("personaje").insertOne(myobj, function(err, res) {
     if (err) throw err;
     
@@ -23,7 +25,7 @@ app.get('/traerPersonaje',function(){
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    dbo.collection("personaje").findOne({}, function(err, result) {
+    dbo.collection("personaje").findAll({}, function(err, result) {
       if (err) throw err;
       
       db.close();
