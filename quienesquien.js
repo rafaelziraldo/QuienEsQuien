d86739fb0a0809ff790aa6c9ce517407635b21c3
fedeziraldo <federrico04@gmail.com>
2018-11-17 17:42:29 -0300

    cosas

--- quienesquien.js
+++ quienesquien.js
@@ -11,29 +11,51 @@ app.get('/', function(req, res) {
 
 //Whenever someone connects this gets executed
 let users = 0
-io.on('connection', function(socket) {
+io.on('connection', socket =>{
    console.log('A user connected')
    users++
    if (users == 1){
        io.emit('esperando', {})
    }else if(users == 2){
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("quien");
-            dbo.collection("personaje").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            
-            db.close();
-            io.emit('empieza', result)
-            });
-        });
+      MongoClient.connect(url, (err, db) =>{
+         if (err) throw err;
+         var dbo = db.db("quien");
+         dbo.collection("personaje").find({}).toArray((err, result) =>{
+         if (err) throw err
+         
+         db.close()
+         io.emit('empieza', result)
+         });
+      });
    }
+
+   socket.on('pregunta', preg =>{
+      io.broadcast('pregunta', preg)
+   })
+
+   socket.on('respuesta', resp =>{
+      io.broadcast('respuesta', resp)
+   })
+
+   socket.on('gano', () =>{
+      io.broadcast('gano')
+   })
+
+   socket.on('arriesga', arr =>{
+      io.broadcast('arriesga', arr)
+   })
+
+   socket.on('arriesgaResp', arrR =>{
+      io.broadcast('arriesgaResp', arrR)
+   })
+
    //Whenever someone disconnects this piece of code executed
-   socket.on('disconnect', function () {
+   socket.on('disconnect', () =>{
+      io.broadcast('gano')
       console.log('A user disconnected')
    })
 })
 
-http.listen(3000, function() {
+http.listen(3000, () =>{
    console.log('listening on *:3000')
 })
